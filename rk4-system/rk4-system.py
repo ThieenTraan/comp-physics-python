04a50667e890b29486ea56b0122b4c002a802038
Harry Huy <harryhuydoan@gmail.com>
2017-04-09 21:28:19 +0700

    rk4 system for 2 equs

--- rk4-system/rk4-system.py
+++ rk4-system/rk4-system.py
@@ -1,50 +1,68 @@
 #!/usr/bin/python3
+import numpy
 
 eq_num = 2
-init_file = "init-val.txt"
-coeff_file = "coeff-val.txt"
-result_file = "result.txt"
-init_val = []
-coeff_val = []
-h
-x0
-y0
-xn
 
-n = int((xn - x0)/h)
+# init_file = "init-val.txt"
+# coeff_file = "coeff-val.txt"
+# result_file = "result.txt"
 
-def get_init_val():
-	global init_val
-	file = open(init_file, "r")
-	line = file.read()
-	init_val = int(t) for t in line.split()
-	file.close()
-get_init_val()
-# print(init_val)
+# y0
+# init_val = [6, 4]
 
-def get_coeff_val():
-	global coeff_val
-	file = open(coeff_file, "r")
-	for i in range(eq_num):
-		line = file.readline()
-		coeff_val.append(int(t) for t in line.split())
-get_coeff_val()
-# print(coeff_val[1][1])
+# coeff_val = []
+
+t0 = 0
+tn = 1
+h = 0.5
+
+func = []
+# func.append(lambda y1,y2: y1 + 2*y2)
+# func.append(lambda y1,y2: 3*y1 + 2*y2)
+func.append(lambda y1,y2: -0.5*y1)
+func.append(lambda y1,y2: 4 - 0.1*y1 - 0.3*y2)
+
+n = int((tn - t0)/h)
+t = numpy.arange(t0, tn+h, h)
+y1 = [4]
+y2 = [6]
+print(t)
+print(n)
+
+# def get_init_val():
+# 	global init_val
+# 	file = open(init_file, "r")
+# 	line = file.read()
+# 	init_val = int(t) for t in line.split()
+# 	file.close()
+# get_init_val()
+# # print(init_val)
+
+# def get_coeff_val():
+# 	global coeff_val
+# 	file = open(coeff_file, "r")
+# 	for i in range(eq_num):
+# 		line = file.readline()
+# 		coeff_val.append(int(t) for t in line.split())
+# get_coeff_val()
+# # print(coeff_val[1][1])
 
 
-def func(eq):
-	val = int(coeff_val[eq][0])
-	for i in range(1,eq_num):
-		val = val + coeff_val[eq][i]*init_val[i]
-	return val
-# print(func(1))
-
-def rk4(eq):
-	f = func(eq)
-	k1
-	k2
-	k3
-	k4
-	y
-
+# f = func[eq]
+# y = [init_val[eq]]
+for i in range(n):
+	k1 = h*func[0](y1[i], y2[i])
+	l1 = h*func[1](y1[i], y2[i])
+	k2 = h*func[0](y1[i] + k1/2, y2[i] + l1/2)
+	l2 = h*func[1](y1[i] + k1/2, y2[i] + l1/2)
+	k3 = h*func[0](y1[i] + k2/2, y2[i] + l2/2)
+	l3 = h*func[1](y1[i] + k2/2, y2[i] + l2/2)
+	k4 = h*func[0](y1[i] + k3, y2[i] + l3)
+	l4 = h*func[1](y1[i] + k3, y2[i] + l3)
+	y1.append(y1[i] + 1/6*(k1 + 2*k2 + 2*k3 + k4))
+	y2.append(y2[i] + 1/6*(k1 + 2*k2 + 2*k3 + k4))
+	print(k1,k2,k3,k4)
+	print(l1,l2,l3,l4)
+print(y1)
+print(y2)
 # def export_result():
--- rk4/rk4.py
+++ rk4/rk4.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import math
-# import numpy
+import numpy
 
 def func(x,y):
-	return (x-y)/2
+	return 3*x + 2*y
 
 x0 = 0
 xn = 1
-h = 0.5
-y0 = 1
+h = 0.2
+y0 = 4
 
 n = int((xn-x0)/h)
 
 y = [y0]
-x = [0,0.5,1]
+x = numpy.arange(x0, xn+h, h)
 
 for i in range(n):
 	k1 = h*func(x[i], y[i])
@@ -22,7 +22,8 @@ for i in range(n):
 	k4 = h*func(x[i] + h, y[i] + k3)
 	y.append(y[i] + 1/6*(k1 + 2*k2 + 2*k3 + k4))
 
-data_result = open("result.txt", "w")
-for i in range(n+1):
-	data_result.write("{0:3}    {1:.4f}\n".format(x[i], y[i]))
-data_result.close()
+print(y)
+# data_result = open("result.txt", "w")
+# for i in range(n+1):
+# 	data_result.write("{0:3}    {1:.4f}\n".format(x[i], y[i]))
+# data_result.close()
