5ce6885df034fccb9120814895a1ec139028deb7
Harry Huy <harryhuydoan@gmail.com>
2017-04-10 09:57:43 +0700

    rk4-system testing

--- rk4-system/rk4-system.py
+++ rk4-system/rk4-system.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 import numpy
+import pprint
 
 # define number of euations
 eq_num = 2
 
 # init_file = "init-val.txt"
 # coeff_file = "coeff-val.txt"
-# result_file = "result.txt"
+result_file = "result.txt"
 # coeff_val = []
 
 t0 = 0
 tn = 1
-h = 0.5
+h = 0.25
 
 f = []
 
 # function type: f(*args) = f(t,y0,y1,...,yn)
-f.append(lambda *args: -0.5*args[1])
-f.append(lambda *args: 4 - 0.1*args[1] - 0.3*args[2])
+f.append(lambda *args: -4*args[1] + 3*args[2] + 6)
+f.append(lambda *args: -2.4*args[1] + 1.6*args[2] + 3.6)
 
 n = int((tn - t0)/h)
 t = numpy.arange(t0, tn+h, h)
-y = []
-y.append([4])
-y.append([6])
+y = [[0], [0]]
 
-print(t)
-print(n)
+print(*t,sep="\t")
 
 # def get_init_val():
 # 	global init_val
@@ -46,7 +44,7 @@ print(n)
 # get_coeff_val()
 # # print(coeff_val[1][1])
 
-# i is step index
+# i is rk4 step index
 # j is k[1,4] index
 # k is y index
 for i in range(n):
@@ -81,5 +79,14 @@ for i in range(n):
 		y[k].append(y[k][i] + 1/6*(k1[k] + 2*k2[k] + 2*k3[k] + k4[k]))
 
 for i in range(eq_num):
-	print(y[i])
-# def export_result():
+	print(*y[i], sep="\t")
+def export_result():
+	f = open("result-file.txt", "w")
+	line = ""
+	for i in range(n):
+		line = line + str(t[i]) + " "
+		line = line + str(y[0][i]) + " " + str(y[1][i]) + "\n"
+	f.write(line)
+	f.close()
+
+export_result()
